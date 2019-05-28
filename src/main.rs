813252f5aa45f464a6fd9a5a125ca65d07a3d4ad
KoStard <rub099999720@gmail.com>
2019-05-28 17:10:56 +0400

    Fixing bugs and compiling the game.

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,36 @@ use std::io;
 
 #[cfg(not(test))]
 fn main() {
-    let total_miss = 5u;
+    let total_miss = 5u8;
     
     let secret_word = get_secret_word();
 
-    println!("The secret word is: {}", secret_word);
-
-    // TODO: use regular expression in case of whitespaces, dashes, etc.
-    let mut guessing_word = String::from_char(secret_word.len(), '_');
+    let mut guessing_word = secret_word.chars().map(|c|if c != ' ' {'_'} else {' '}).collect::<String>();
 
     let mut guessed_letters: Vec<char> = vec![];
 
-    let mut count = 1;
+    let mut count = 0;
     
     loop {
         println!("Type your guess!");
-        let input = io::stdin().read_line().ok().expect("Failed to read line");
-        let input_char: char = input.char_at(0);
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Did not enter a correct string");
+        if let Some('\n')=input.chars().next_back() {
+            input.pop();
+        }
+        if let Some('\r')=input.chars().next_back() {
+            input.pop();
+        }
+        let input_char: char = input.chars().next().unwrap();
         if guessed_letters.contains(&input_char) {
             println!("You already guessed it.");
         } else {
             let indexes = get_letter_indexes(input_char, secret_word);
             if indexes.is_empty() {
-                count += 1u;
-                println!("NAH NAH! Letter not found.");
+                count += 1;
+                if total_miss-count != 0 {
+                    println!("NAH NAH! Letter not found. You have {} more lives.", total_miss-count);
+                }
             } else {
                 println!("Letter was found.");
                 replace_letters(indexes.clone(), &mut guessing_word);
@@ -34,7 +40,7 @@ fn main() {
             if count == total_miss { break; }
             guessed_letters.push(input_char);
         }
-        if guessing_word.as_slice() == secret_word { println!("You win!"); return; }
+        if &guessing_word[..] == secret_word { println!("You win!"); return; }
     }
     println!("You lose :-(");
 }
@@ -45,11 +51,11 @@ fn get_secret_word() -> &'static str {
 }
 
 
-fn get_letter_indexes(letter: char, secret_word: &str) -> Vec<(uint, char)> {
+fn get_letter_indexes(letter: char, secret_word: &str) -> Vec<(usize, char)> {
     secret_word.chars().enumerate().filter(|&(_, l)| l == letter).collect::<Vec<_>>()
 }
 
-fn replace_letters(indexes: Vec<(uint, char)>, secret_word: &mut String) {
+fn replace_letters(indexes: Vec<(usize, char)>, secret_word: &mut String) {
     for &(idx, letter) in indexes.iter() {
         secret_word.remove(idx);
         secret_word.insert(idx, letter);
@@ -59,6 +65,7 @@ fn replace_letters(indexes: Vec<(uint, char)>, secret_word: &mut String) {
 
 #[test]
 fn test_get_secret_word() {
+    //- Invalid test - working only for one word!
     let secret_word = get_secret_word();
     assert_eq!("apple", secret_word);
 }
